d77391d796a976762681fcde57e07d2a2281c19a
amharrawood <120509907+amharrawood@users.noreply.github.com>
2026-04-17 02:36:40 +0000

    Initial setup adding files

---
+++ AnimalShelterProject/AnimalShelter/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AnimalShelter;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please select mode (animal OR schedule): ");
+        string mode = Console.ReadLine();
+        if(mode=="animal") {
+            
+            string command;
+            do {
+                Console.Write("Enter animal name: ");
+                string animalName = Console.ReadLine();
+                
+                Console.Write("Enter animal health information: ");
+                string health = Console.ReadLine();
+                
+                File.AppendAllText("animal-data.txt",animalName+":"+health+Environment.NewLine);
+                
+                Console.Write("Enter command (end OR continue): ");
+                command = Console.ReadLine();
+            } while(command!="end");
+        }
+    }
+}
